81d42b656a53c5ebc80721a2bd12dce9186a3da9
Mr. Pine <50425705+Mr-Pine@users.noreply.github.com>
2022-12-28 12:00:03 +0100

    Rework build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,37 +11,32 @@ repositories {
     mavenCentral()
 }
 
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+}
+
 // Configure Gradle IntelliJ Plugin
 // Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
 intellij {
     version.set("2022.1.4")
-    type.set("IC") // Target IDE Platform
-
-    plugins.set(listOf(/* Plugin Dependencies */))
 }
 
 tasks {
-    // Set the JVM compatibility versions
-    withType<JavaCompile> {
-        sourceCompatibility = "11"
-        targetCompatibility = "11"
-    }
-    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-        kotlinOptions.jvmTarget = "11"
+    buildSearchableOptions {
+        enabled = false
     }
 
     patchPluginXml {
+        version.set("${project.version}")
         sinceBuild.set("221")
         untilBuild.set("231.*")
     }
 
-    signPlugin {
-        certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
-        privateKey.set(System.getenv("PRIVATE_KEY"))
-        password.set(System.getenv("PRIVATE_KEY_PASSWORD"))
+    compileKotlin {
+        kotlinOptions.jvmTarget = "11"
     }
 
-    publishPlugin {
-        token.set(System.getenv("PUBLISH_TOKEN"))
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "11"
     }
 }
