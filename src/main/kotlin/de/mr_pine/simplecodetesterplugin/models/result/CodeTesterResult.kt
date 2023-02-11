3139609637cc625aa43d4708eef9bdad887ab202
Mr. Pine <50425705+Mr-Pine@users.noreply.github.com>
2023-02-11 19:21:10 +0100

    V 1.2.1 Doesn't crash anymore if compilation is successful

--- src/main/kotlin/de/mr_pine/simplecodetesterplugin/models/result/CodeTesterResult.kt
+++ src/main/kotlin/de/mr_pine/simplecodetesterplugin/models/result/CodeTesterResult.kt
@@ -1,8 +1,10 @@
 package de.mr_pine.simplecodetesterplugin.models.result
 
+import kotlinx.serialization.Serializable
 import kotlinx.serialization.Transient
 import kotlin.time.Duration
 
+@Serializable
 data class CodeTesterResult(
     val fileResults: Map<String, List<CheckResult>>? = null,
     val compilationOutput: CompilationOutput
